b2d6b0429ef996aab1b96c24bf1fe8c6aff94106
Zamiralov Ivan <zamiralovivan5@gmail.com>
2022-08-23 15:13:16 +0300

    add run animation

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function Star(){
   let self = this;
-  const container = document.getElementById('container')
   
   self.initStar = function(){
     for(let i = 1; i < 31; i++){
@@ -24,5 +23,108 @@ function Star(){
   }
 }
 
+
+
+function Player(){
+  const self = this;
+  let img = document.getElementById('player');
+  let src = null;
+  let idx = 1;
+  let timermove = null
+  let posX = img.offsetLeft;
+  let posY = 67;
+  let run = null;
+  let jumpIdx = 1;
+
+  self.updateSoure = function(){
+    idx >= 5? idx = 1:idx++;
+    src = `./IMG/Player/run/${idx}.png`;
+    self.updateImg();
+  }
+
+  self.updateImg = function(){
+    img.setAttribute('src',src);
+  }
+
+  self.startRun = function(){
+    run = setInterval(self.updateSoure,75)
+  }
+
+  self.move = function(e){
+    console.log(e.keyCode)
+    switch(e.keyCode){
+      case 39: timermove = requestAnimationFrame(self.moveRight);
+        break;
+      case 37: timermove = requestAnimationFrame(self.moveLeft);
+        break;
+      case 38:timer = requestAnimationFrame(self.jump);
+      break;
+    }
+  }
+  self.moveRight = function(){
+    cancelAnimationFrame(timermove);
+    posX+=1;
+    img.style.transform = `translate(${posX}px,${posY}vh)`
+    timermove = requestAnimationFrame(self.moveRight)
+  }
+
+  self.moveLeft = function(){
+    cancelAnimationFrame(timermove);
+    posX+=-1;
+    img.style.transform = `translate(${posX}px,${posY}vh)`
+    timermove = requestAnimationFrame(self.moveLeft)
+  }
+
+  self.jumpAnimation = function(){
+    self.jumpUpdateSrc();
+    self.jumpUpdateCoords();
+    jumpIdx++;
+  }
+
+  self.jumpUpdateSrc = function(){
+    const jumpSrc = `./IMG/Player/jump/${jumpIdx}.png`;
+    self.jumpUpdateImg(jumpSrc);
+  }
+
+  self.jumpUpdateImg = function(jumpSrc){
+    img.setAttribute('src',jumpSrc);
+  }
+  
+  self.jumpUpdateCoords = function(){
+    if(jumpIdx >=2 && jumpIdx <=5){
+      posY+=-5;
+    }
+    else if (jumpIdx >=5 && jumpIdx <10){
+      posY+=5;
+    }
+    img.style.transform = `translate(${posX}px,${posY}vh)`;
+  }
+
+
+  self.jump = function(){
+    clearInterval(run);
+    cancelAnimationFrame(self.jump)
+      if(jumpIdx <= 10){
+        self.jumpAnimation();
+       setTimeout(()=>timer = requestAnimationFrame(self.jump),100);
+      }
+      else{
+        run = setInterval(self.updateSoure,75)
+        jumpIdx =1;
+        posY = 67;
+      }         
+  }
+
+  self.stopMove = function(){
+    cancelAnimationFrame(timermove);
+  }
+}
+
+const container = document.getElementById('container')
 const stars = new Star();
 stars.initStar();
+const player = new Player();
+player.startRun();
+
+document.addEventListener('keydown',player.move)
+document.addEventListener('keyup',player.stopMove)
